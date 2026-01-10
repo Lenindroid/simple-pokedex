b11455717bc61006e5ee3f0ccda7f1798006f94f
Lenin Rojas Sánchez <leninrojassanchez09@gmail.com>
2026-01-09 23:28:38 -0600

    preview function when typing

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,12 +2,20 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 function Search() {
+  function preview() {}
+
   return (
     <div className="maximum-width search-bar">
       <label className="sr-only" for="site-search">
         Search the pokemon by its name:
       </label>
-      <input type="search" placeholder="Lapras" id="site-search" name="q" />
+      <input
+        type="search"
+        placeholder="Lapras"
+        id="site-search"
+        name="q"
+        onChange={preview}
+      />
       <button>
         <FontAwesomeIcon icon={faMagnifyingGlass} />
       </button>
